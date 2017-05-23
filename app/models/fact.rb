852d1c6d12b97398c9b65a079d60cb71b6ebd6fd
Ray Meibaum <rmeibaum@me.com>
2017-05-22 21:49:20 -0400

    refine response

--- app/models/fact.rb
+++ app/models/fact.rb
@@ -1,9 +1,9 @@
 class Fact < ApplicationRecord
   def self.get_certain_number(number)
-    order("RANDOM()").limit(number)
+    order("RANDOM()").limit(number).select("id, content")
   end
 
   def self.get_all
-    order("RANDOM()").all
+    order("RANDOM()").all.select("id, content")
   end
 end
